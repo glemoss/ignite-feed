75ef8f08267be2ae8b35faa81737857945a8dc51
Gabriel Lemos <glemoss@yahoo.com>
2024-04-16 14:12:45 -0300

    sidebar component

--- react/01-fundamentos-reactjs/src/App.jsx
+++ react/01-fundamentos-reactjs/src/App.jsx
@@ -1,17 +1,26 @@
 import { Post } from './components/Post'
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 
 import './global.css'
+import styles from './App.module.css'
 
 export function App() {
 
   return (
     <>
-      <Header/>
-      <Post 
-        author="Gabriel Lemos" 
-        content="Tempor eiusmod irure commodo consequat cupidatat est excepteur esse labore mollit proident ex id laboris."
-      />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar name="Gabriel Lemos" job="Fullstack Developer"/>
+        
+        <main>
+          <Post 
+            author="Gabriel Lemos" 
+            content="Tempor eiusmod irure commodo consequat cupidatat est excepteur esse labore mollit proident ex id laboris."
+          />
+        </main>
+      </div>
     </>
   )
 }
