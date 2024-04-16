64c6ff7936c39e8532bc1b3750c7b9c4895b981a
Gabriel Lemos <glemoss@yahoo.com>
2024-04-16 10:57:05 -0300

    set global styles

--- react/01-fundamentos-reactjs/src/App.jsx
+++ react/01-fundamentos-reactjs/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Post } from './components/Post'
+import { Header } from './components/Header'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './global.css'
+
+export function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header/>
+      <Post 
+        author="Gabriel Lemos" 
+        content="Tempor eiusmod irure commodo consequat cupidatat est excepteur esse labore mollit proident ex id laboris."
+      />
     </>
   )
 }
-
-export default App
